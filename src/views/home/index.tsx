b205ff480c47892fbcbee76c06809f7f6876476c
kaelnew <kevin@rentfun.io>
2023-07-25 22:08:39 +0800

    ...

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -191,7 +191,7 @@ const t5Data: t5Block[] = [
     },
     {
         icon: require('./images/s5/10.png'),
-        url: 'https://twitter.com/_dashred',
+        url: 'https://twitter.com/dash123d',
     },
     {
         icon: require('./images/s5/11.png'),
